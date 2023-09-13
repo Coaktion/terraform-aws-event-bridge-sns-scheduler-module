543a8f1be12e025fc89196ddf0f179b869926f1e
gustavo <gus.inacio@hotmail.com>
2023-09-13 18:23:17 -0300

    chore: add example

---
+++ example/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = ""
+  access_key = ""
+  secret_key = "" 
+}
+
+module "event-bridge-sns-scheduler" {
+  source = "github.com/inaciogu/event-bridge-sns-scheduler"
+  schedulers = [ 
+    {
+      name = "test-scheduler"
+      description = "This is a test scheduler"
+      schedule_expression = "cron(53 17 * * ? *)"
+      mode = "OFF"
+      sns_topic_arn = "arn:aws:sns:us-east-1:800352480120:new_candidate"
+      input = "This is a test message"
+      timezone = "America/Sao_Paulo"
+    }
+  ]
+}
